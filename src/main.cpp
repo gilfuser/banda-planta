1483e91064b23071df3639d50222dbc0b02f424a
Gil Fuser <gilfuser@gmail.com>
2022-02-22 00:42:36 -0300

    gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
 
+int sensorPin = A9;
+int sensorValue = 0;
+int outputValue = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode( LED_BUILTIN, OUTPUT );
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  sensorValue = analogRead(sensorPin);
+  // outputValue = map( sensorValue, 0, 255, 0, 255);
+  analogWrite(LED_BUILTIN, outputValue);
+  Serial.print("sensor = ");
+  Serial.print(sensorValue);
+  Serial.print("\t output = ");
+  Serial.println(outputValue);
+  delay(2);
 }
