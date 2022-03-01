90015af8c6edc01655f0c04eca60adc8ac55671e
Gil Fuser <gilfuser@gmail.com>
2022-03-01 19:53:18 -0300

    pre 555 timer chip

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,9 @@
 #include <WiFiClient.h>
 #include <WiFiUdp.h>
 #include <OSCMessage.h> // instalar via IDE
-// #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
-
 
 int sensorPin = A0;
 int sensorValue = 0;
-// int outputValue = 0;
 
 const char* ssid = "Popoloninis"; // VARIÁVEL QUE ARMAZENA O NOME DA REDE SEM FIO
 const char* password = "cibpopfu5"; // 24378Skmecs VARIÁVEL QUE ARMAZENA A SENHA DA REDE SEM FIO
@@ -34,12 +31,12 @@ const IPAddress outIp(192, 168, 0, 55);  // ENDEREÇO IP DO COMPUTADOR
 
 /////////////////////////////////////////////////////////////////////
 
-const unsigned int outPort = 57120;         // remote port to receive OSC
+const unsigned int outPort = 57121;         // remote port to receive OSC
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-const unsigned int localPort = 8888;      // local port to listen for OSC packets (actually not used for sending)
+const unsigned int localPort = 8889;      // local port to listen for OSC packets (actually not used for sending)
 
 // DEFINIÇÃO DE IP FIXO PARA O NODEMCU
 IPAddress ip(192,168,0,106); //COLOQUE UMA FAIXA DE IP DISPONÍVEL DO SEU ROTEADOR. EX: 192.168.1.110 **** ISSO VARIA, NO MEU CASO É: 192.168.0.175
@@ -50,56 +47,39 @@ WiFiServer server(80); //CASO OCORRA PROBLEMAS COM A PORTA 80, UTILIZE OUTRA (EX
 
 void setup() {
     // put your setup code here, to run once:
-    // Serial.begin(115200);
-
-    //WiFiManager
-    //Local intialization. Once its business is done, there is no need to keep it around
-    // WiFiManager wifiManager;
-    //reset saved settings
-    //wifiManager.resetSettings();
+    Serial.begin(115200);
 
     pinMode(sensorPin, INPUT);
     
-    //set custom ip for portal
-    // wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
-
-    //fetches ssid and pass from eeprom and tries to connect
-    //if it does not connect it starts an access point with the specified name
-    //here  "AutoConnectAP"
-    //and goes into a blocking loop awaiting configuration
-    // wifiManager.autoConnect("AutoConnectAP");
-    //or use this for auto generated name ESP + ChipID
-    //wifiManager.autoConnect();
-
   WiFi.mode(WIFI_STA);
-  WiFi.hostname("esposc");
-  delay(1000);
-  // Serial.println();
-  // Serial.println();
-  // Serial.print("Connecting to ");
-  // Serial.println(ssid);
+  WiFi.hostname("espoca");
+  delay(2000);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    // Serial.print(".");
+    Serial.print(".");
   }
 
-  //Udp.begin(localPort);
-  //Serial.print("Local port: ");
-  //Serial.println(Udp.localPort());
+  Udp.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(Udp.localPort());
 
-  // Serial.print(F("WiFi connected! IP address: "));
-  // Serial.println(WiFi.localIP());
+  Serial.print(F("WiFi connected! IP address: "));
+  Serial.println(WiFi.localIP());
     
     //if you get here you have connected to the WiFi
-    // Serial.println("connected...yeey :)");
+    Serial.println("connected...yeey :)");
 }
 // fim de setup()
 
 void loop() {
   sensorValue = analogRead(sensorPin);
-    OSCMessage msg("/bromelia");
+    OSCMessage msg("/suculenta");
     msg.add(sensorValue);
     Udp.beginPacket(outIp, outPort);
     msg.send(Udp);
