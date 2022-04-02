ba73f67579533148ecbafaaf8b122fb6bd290e93
Gil Fuser <9404458+gilfuser@users.noreply.github.com>
2022-04-01 23:58:51 -0300

    cleaning code

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,8 @@ volatile byte ind = 0;
 volatile word samples[samplesize];
 float threshold = 0.1;   //2.3;  //change threshold multiplier
 
-const char* ssid = "Popoloninis"; // VARIÁVEL QUE ARMAZENA O NOME DA REDE SEM FIO
-const char* password = "cibpopfu5"; // 24378Skmecs VARIÁVEL QUE ARMAZENA A SENHA DA REDE SEM FIO
+const char* ssid = "rede_miceliar"; // VARIÁVEL QUE ARMAZENA O NOME DA REDE SEM FIO
+const char* password = "2478Esta"; // 24378Skmecs VARIÁVEL QUE ARMAZENA A SENHA DA REDE SEM FIO
 
 WiFiUDP Udp;
 
@@ -27,25 +27,25 @@ WiFiUDP Udp;
 //          QUI MUDE AQUI MUDE AQUI MUD
 //              E AQUI MUDE AQUI MUDE AQ
 //                  QUI MUDE AQUI MUDE A
-//                      QUI MUDE AQUI MU
-//                           DE AQUI MU
-//                                DE A
-//                                 Q
+//                      QUI MUDE AQUI M
+//                           UDE AQUI M
+//                                UDE A
+//                                  QU
 
-const IPAddress outIp(192, 168, 0, 55);  // ENDEREÇO IP DO COMPUTADOR
+const IPAddress outIp(192,168,1,103);  // ENDEREÇO IP DO COMPUTADOR
 
 /////////////////////////////////////////////////////////////////////
 
-const unsigned int outPort = 57120;         // remote port to receive OSC
+const unsigned int outPort = 57121;         // remote port to receive OSC
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-const unsigned int localPort = 8889;      // local port to listen for OSC packets (actually not used for sending)
+const unsigned int localPort = 8890;      // local port to listen for OSC packets (actually not used for sending)
 
 // DEFINIÇÃO DE IP FIXO PARA O NODEMCU
-IPAddress ip(192,168,0,106); //COLOQUE UMA FAIXA DE IP DISPONÍVEL DO SEU ROTEADOR. EX: 192.168.1.110 **** ISSO VARIA, NO MEU CASO É: 192.168.0.175
-IPAddress gateway(192,168,0,1); //GATEWAY DE CONEXÃO (ALTERE PARA O GATEWAY DO SEU ROTEADOR)
+IPAddress ip(192,168,1,255); //COLOQUE UMA FAIXA DE IP DISPONÍVEL DO SEU ROTEADOR. EX: 192.168.1.110 **** ISSO VARIA, NO MEU CASO É: 192.168.0.175
+IPAddress gateway(192,168,1,1); //GATEWAY DE CONEXÃO (ALTERE PARA O GATEWAY DO SEU ROTEADOR)
 IPAddress subnet(255,255,255,0); //MASCARA DE REDE
 WiFiServer server(80); //CASO OCORRA PROBLEMAS COM A PORTA 80, UTILIZE OUTRA (EX:8082,8089) E A CHAMADA DA URL FIC
 
@@ -54,15 +54,15 @@ void analyzeSample();
 
 void setup() {
     // put your setup code here, to run once:
-    // Serial.begin(115200);
+    Serial.begin(115200);
 
   WiFi.mode(WIFI_STA);
-  WiFi.hostname("espirro");
+  WiFi.hostname("floresta");
   delay(2000);
-  // Serial.println();
-  // Serial.println();
-  // Serial.print("Connecting to ");
-  // Serial.println(ssid);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -71,26 +71,35 @@ void setup() {
   }
 
   Udp.begin(localPort);
-  // Serial.print("Local port: ");
-  // Serial.println(Udp.localPort());
+  Serial.print("Local port: ");
+  Serial.println(Udp.localPort());
 
-  // Serial.print(F("WiFi connected! IP address: "));
-  // Serial.println(WiFi.localIP());
+  Serial.print(F("WiFi connected! IP address: "));
+  Serial.println(WiFi.localIP());
     
   //   //if you get here you have conected to the WiFi
-    // Serial.println("connected...yeey :)");
+    Serial.println("connected...yeey :)");
     attachInterrupt(interruptPin, sample, CHANGE);  //begin sampling from interrupt
 }
 // fim de setup()
 
 word value = 0;
 unsigned long antes = 0;
-const byte intervalo = 3;
+const byte intervalo = 10;
 
 void loop() {
-  // unsigned long agora = millis();
+  unsigned long agora = millis();
+    // delay(500);
   if( ind >= samplesize )  { analyzeSample(); }
-  // if ( agora - antes >= intervalo )  { antes = agora;  }
+  if ( agora - antes >= intervalo )  { antes = agora; 
+    OSCMessage msg("/suculenta");
+    msg.add(value);
+    Udp.beginPacket(outIp, outPort);
+    msg.send(Udp);
+    Udp.endPacket();
+    msg.empty();
+    // Serial.println(value);
+  }
 }
 
 //interrupt timing sample array
@@ -137,13 +146,7 @@ void analyzeSample()
     //*********
     if(change){// set note and control vector
       value = averg;
-    OSCMessage msg("/suculenta");
-    msg.add(value);
-    Udp.beginPacket(outIp, outPort);
-    msg.send(Udp);
-    Udp.endPacket();
-    msg.empty();
-      // Serial.println(value);
+    // Serial.println(value);
      }
      //reset array for next sample
       ind = 0;
